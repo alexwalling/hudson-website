813bdd3d8a78a48280db9399d7992d250996470e
alexwalling <alex.walling19@gmail.com>
2017-07-01 11:29:46 -0700

    cleaning up and rearanging pics

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,23 @@ class App extends Component {
 		return (
 			<div>
 				<Grid>
-					<MyRow img1='/img/Facebook-Card.jpg' img2='/img/YouTube-Card.jpg' img3='/img/Twitter-Card.jpg' 
-							title1='Facebook' title2='Youtube' title3='Twitter'
-							info1='Follow my posts, videos and photos on Facebook.' info2='My personal youtube channel filled with my travel vlogs and films.' info3='Hear any new or updates I have first! Join in on discussion and get in my DMs.'
-							link1='https://www.facebook.com/james.hudson.ratz/' link2='https://www.youtube.com/c/JamesHudsonRatz' link3='https://twitter.com/jameshudratz'
+					<MyRow img1='/img/Welcome-Card.jpg' 										img2='/img/YouTube-Card.jpg' 												img3='/img/Instagram-Card.jpg' 
+							title1='Welcome' 													title2='Youtube' 															title3='Instagram'
+							info1='Welcome to the hub of all my work! Take a look around :)' 	info2='My personal youtube channel filled with my travel vlogs and films.' 	info3='View my photography portfolio and follow my day on insta stories.'
+							link1='mailto:james.hudratz@gmail.com' 								link2='https://www.youtube.com/c/JamesHudsonRatz' 							link3='https://www.instagram.com/james.hudson.ratz/'
 							/>
 					<br/>
-					<MyRow img1='/img/Kit-Card.jpg' img2='/img/Instagram-Card.jpg' img3='/img/Medium-Card.jpg'
-							title1='Kit' title2='Instagram' title3='Medium'
-							info1='Where I list all the cool stuff I find useful and the gear I use.' info2='View my photography portfolio and follow my day on insta stories.' info3='Where I like to write about my travels, adventures, and experiences.'
-							link1='https://kit.com/JamesHudson' link2='https://www.instagram.com/james.hudson.ratz/' link3='https://medium.com/@james.hudson'
+					<MyRow img1='/img/Medium-Card.jpg' 														img2='/img/Twitter-Card.jpg' 																img3='/img/Kit-Card.jpg' 
+							title1='Medium' 																title2='Twiiter' 																			title3='Kit'
+							info1='Where I like to write about my travels, adventures, and experiences.' 	info2='Hear any new or updates I have first! Join in on discussion and get in my DMs.' 		info3='Where I list all the cool stuff I find useful and the gear I use.'
+							link1='https://medium.com/@james.hudson' 										link2='https://twitter.com/jameshudratz' 													link3='https://kit.com/JamesHudson'
 							/>
+					<br/>
+					<MyRow img1='/img/Facebook-Card.jpg' 														img2='' 		img3=''
+							title1='Facebook' 																	title2='' 		title3=''
+							info1='Follow my posts, videos and photos on Facebook.' 							info2='' 		info3=''
+							link1='https://www.facebook.com/james.hudson.ratz/' 								link2='' 		link3=''
+					/>
 				</Grid>
 			</div>
 		);
